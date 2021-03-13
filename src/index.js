9040792bd19a02cf70549bccf9a2b95f717ab469
parth-vedaha <parth.g@vedaha.com>
2021-03-13 12:37:50 +0530

    init

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 function useDebounce(callback, delay) {
   const debouncedFn = useCallback(
     debounce((...args) => callback(...args), delay),
-    [delay] // will recreate if delay changes
+    [delay]
   )
   return debouncedFn
 }
